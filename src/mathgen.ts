73654834edbfbabf548aa6e026f15f7c40647794
Azer Sadykhzadeh <51178055+Sadykhzadeh@users.noreply.github.com>
2022-01-03 02:13:23 +0400

    add brackets support

--- src/mathgen.ts
+++ src/mathgen.ts
@@ -14,6 +14,15 @@ export const mathGen = (
 } => {
   const answer = more?.answer ? more.answer : genNumber(10, 20);
   const expression: Array<number | '+' | '-' | '*' | '/' | string> = genExpression(answer, length);
+  if (more?.brackets) {
+    const evenInds = Array.from(Array(expression.length).keys()).filter(i => i % 2 == 0)
+    let randInd = evenInds[genNumber(0, evenInds.length - 1)];
+    const bracketsEx = (genExpression(
+      expression[randInd] as number,
+      genNumber(2, 3)
+    ).join("") as string).replaceAll("--", "+").replaceAll("+-", "-").replaceAll("-+", "-");
+    expression[randInd] = `(${bracketsEx})`;
+  }
   return {
     "task": expression,
     "answer": answer
